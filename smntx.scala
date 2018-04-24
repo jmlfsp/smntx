6cc60a97f3c74c3b689cb230ae47c7e4dd71555d
jmlfsp <37919694+jmlfsp@users.noreply.github.com>
2018-04-24 11:45:32 -0300

    Update smntx.scala

--- smntx.scala
+++ smntx.scala
@@ -7,48 +7,13 @@ import scala.util.matching.Regex
 object App {
   def main(args: Array[String]): Unit = {
 
-    val conf = new SparkConf().setAppName("teste").setMaster("local[*]")
-
-    val sc = new SparkContext(conf)
-
-    val sparkSession = SparkSession.builder()
-      .master("local[*]")
-      .appName("Teste")
-      
-      .enableHiveSupport()
-      .getOrCreate()
-
-    
-    val distData = sc.textFile("file:///home/semantix/Downloads/NASA*")
-    distData.cache()
-
-
-    /*questao 1*/
-    println(distData.map(x => extracao(x,campo="hostname")).distinct().count())
-    
-    val regex_404 = "([\\w|\\.]+)(.*\\[)(\\d{2}/\\w+/\\d{4})(:)(\\d{2}:\\d{2}:\\d{2})(.*)(\\d{4})(] \\\")(.+\\\" )(404)( )(.*)"
-
-    val erro404 = distData.filter(x => x matches regex_404)
-
-    erro404.cache()
-
-    /*questao 2*/
-    print(erro404.count())
-
-    /*questao 3*/
-    erro404.map(x => (extracao(x,campo="hostname"),1)).reduceByKey((x,y) => x+y).map(x=>(x._2->x._1)).sortByKey(ascending=false).take(5).foreach(println(_))
-
-    /*questao 4*/
-    erro404.map( x => (extracao(x, campo= "data"),1)).reduceByKey((x,y) => x+y).collect().foreach(println)
-    
-    /*questao 5*/
-    distData.map(x => (extracao(x,"qtd").toInt)).sum()
+    nasa
 
   }
 
   def extracao(linha: String, campo: String): String = {
 
-    val regex_arq = "([\\w|\\.]+)(.*\\[)(\\d{2}/\\w+/\\d{4})(:)(\\d{2}:\\d{2}:\\d{2})(.*)(\\d{4})(] \\\")(.+\\\" )(\\d{3})( )(.*)".r
+    val regex_arq = "(.+)( - - \\[)(\\d{2}/\\w+/\\d{4})(:)(\\d{2}:\\d{2}:\\d{2})(.*)(\\d{4})(] \\\")(.+\\\" )(\\d{3})( )(.*)".r
     val numero = "(\\d+)".r
 
     linha match {
@@ -57,12 +22,57 @@ object App {
             case "hostname" => hostname
             case "data" => data
             case "qtd" => { qtd_bytes match { case numero(qtd) => qtd; case _ => "0" } }
-            case _ => ""
+            case _ => "_"
         }
       }
-      case _ => ""
+      case _ => "0"
     }
 
   }
 
+  def nasa: Unit = {
+    val conf = new SparkConf().setAppName("teste").setMaster("local[*]")
+
+    val sc = new SparkContext(conf)
+
+    val sparkSession = SparkSession.builder()
+      .master("local[*]")
+      .appName("Teste")
+
+      .enableHiveSupport()
+      .getOrCreate()
+
+    /*Lendo os arquivos de origem*/
+    val distData = sc.textFile("file:///home/semantix/Downloads/NASA*")
+    distData.cache()
+
+    /*questao 1*/
+    println(distData.map(x => extracao(x,campo="hostname")).distinct().count())
+
+
+    /* Filtrando apenas linhas com erro 404*/
+    val regex_404 = "([\\w|\\.]+)(.*\\[)(\\d{2}/\\w+/\\d{4})(:)(\\d{2}:\\d{2}:\\d{2})(.*)(\\d{4})(] \\\")(.+\\\" )(404)( )(.*)"
+
+    val erro404 = distData.filter(x => x matches regex_404)
+    erro404.cache()
+
+    
+    /*questao 2*/
+    print(erro404.count())
+
+
+    /*questao 3*/
+    erro404.map(x => (extracao(x,campo="hostname"),1)).reduceByKey((x,y) => x+y).map(x=>(x._2->x._1)).sortByKey(ascending=false).take(5).foreach(println(_))
+
+
+    /*questao 4 */
+    erro404.map( x => (extracao(x, campo= "data"),1)).reduceByKey((x,y) => x+y).collect().foreach(println)
+
+
+    /*questao 5 */
+    val total_bytes = distData.map(x => extracao(x,"qtd").toInt).sum()
+    println(f"$total_bytes%1.0f")
+
+
+  }
 }
